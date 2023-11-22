f4adf5cd0e7a9bcbba5df619db038ff52da70507
yeqiang_xu <yeqiang_xu@maxio-tech.com>
2023-11-22 09:04:32 +0800

    调整生成的doc logo图标

--- docs/rst/conf.py
+++ docs/rst/conf.py
@@ -224,7 +224,7 @@ html_logo = "./image/aircraft.svg"
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "./image/aircraft.svg"
+html_favicon = "./image/NVMe.jpg"
 
 # This is the file name suffix for HTML files (e.g. ".xhtml").
 #html_file_suffix = '.html'
